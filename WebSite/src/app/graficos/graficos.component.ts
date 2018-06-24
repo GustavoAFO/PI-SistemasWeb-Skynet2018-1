ada5b432363148bd1a312ba9a8a37dd622e4c8b8
GustavoAF <gustavoafonso.gu@gmail.com>
2018-06-24 18:24:41 -0300

    filtro de periodos no grafico

--- WebSite/src/app/graficos/graficos.component.ts
+++ WebSite/src/app/graficos/graficos.component.ts
@@ -13,6 +13,7 @@ export class GraficosComponent implements OnInit, AfterViewInit {
   items: any[];
   dias: any[] = []; // LABELS
   data: any[] = []; // DATA
+  dataPesquisa = { inicial: "", final: "" };
 
   counter = 0;
 
@@ -56,7 +57,42 @@ export class GraficosComponent implements OnInit, AfterViewInit {
   ngOnInit() {
   }
 
+  atualizar() {
+    //console.log("DATA:");
+    //console.log(this.dataPesquisa.inicial);
+    //console.log(this.dataPesquisa.final);
+    //var dateInicial = this.dataPesquisa.inicial.replace(/(\d{2})-(\d{2})-(\d{4})/, "$3/$2/$1");
+    var teste = this.dataPesquisa.inicial.split("-");
+    var dateInicial = teste[0] + "-" + teste[1] + "-" + teste[2];
+
+    teste = this.dataPesquisa.final.split("-");
+    var dateFinal = teste[0] + "-" + teste[1] + "-" + teste[2];
+
+    console.log(dateInicial);
+    console.log(dateFinal);
+
+    this.getAlertas();
+  }
+
+  transformDateSlashTela(data) {
+    var teste = data.split("-");
+    var datett = teste[0] + "/" + teste[1] + "/" + teste[2];
+    return datett;
+  }
+
+  transformDateSlash(data) {
+    var teste = data.split("-");
+    var datett = teste[2] + "/" + teste[1] + "/" + teste[0];
+    return datett;
+  }
+
   getAlertas() {
+
+    this.items = [];
+    this.dias = [];
+    this.data = [];
+
+
     this.db.list('/alertas').snapshotChanges().subscribe((res) => {
 
       res.forEach((tipo) => {
@@ -74,34 +110,87 @@ export class GraficosComponent implements OnInit, AfterViewInit {
 
       res.forEach((pai) => {
 
-        if (!this.dias[pai.key]) {
-          this.dias.push(pai.key);
-          //console.log(this.dias);
+        var inicial = new Date(this.transformDateSlashTela(this.dataPesquisa.inicial));
+        var final = new Date(this.transformDateSlashTela(this.dataPesquisa.final));
+        var datett = this.transformDateSlash(pai.key);
+
+        var attdatt = new Date(datett);
+        //console.log(datett);
+
+        //console.log(this.dataPesquisa.inicial);
+
+        /* console.log("DATA ATUAL:");
+        console.log(attdatt);
+        console.log("DATA INICIAL:");
+        console.log(inicial);
+        console.log("DATA FINAL:");
+        console.log(final);
+        */
+
+        /* console.log(attdatt);
+        if (attdatt <= final) {
+          console.log("DATA ATUAL MENOR QUE A FINAL");
         }
-        // this.dias.push(pai.key);
 
-        this.db.list('/alertas/' + pai.key).snapshotChanges().subscribe(actions => {
-
-          this.counter = 0;
-
-          actions.map(a => {
-
-            this.counter++;
-            // console.log('aumentou');
-
-            const data = a.payload.val();
-            const id = a.payload.key;
+        if (attdatt >= inicial) {
+          console.log("DATA ATUAL MAIOR QUE A INICIAL");
+        } */
 
 
+
+        if (((attdatt <= final) && (attdatt >= inicial)) || (this.dataPesquisa.inicial == "" && this.dataPesquisa.final == "")) {
+          //console.log("ADICIONOU:");
+          //console.log(pai.key);
+
+          if (!this.dias[pai.key]) {
+            this.dias.push(pai.key);
+            //console.log(this.dias);
+          }
+          // this.dias.push(pai.key);
+
+
+          this.db.list('/alertas/' + pai.key).snapshotChanges().subscribe(actions => {
+
+            this.counter = 0;
+
+            actions.map(a => {
+
+              this.counter++;
+              // console.log('aumentou');
+
+              const data = a.payload.val();
+              const id = a.payload.key;
+
+
+
+            });
+
+
+            this.data.push(this.counter);
+            console.log(this.data);
+
+            // console.log(this.counter);
+
+            this.chart.data = {
+              labels: this.dias,
+              datasets: [{
+                label: 'ALERTAS GERADOS ATÉ O MOMENTO',
+                data: this.data,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 10,
+                borderColor: 'rgba(255, 99, 132, 1)'
+              }]
+            };
+
+            this.chart.update();
 
           });
-
-
-          this.data.push(this.counter);
-          console.log(this.data);
-
-          // console.log(this.counter);
-
+        }
+        else {
           this.chart.data = {
             labels: this.dias,
             datasets: [{
@@ -118,8 +207,7 @@ export class GraficosComponent implements OnInit, AfterViewInit {
           };
 
           this.chart.update();
-
-        });
+        }
 
 
       });
