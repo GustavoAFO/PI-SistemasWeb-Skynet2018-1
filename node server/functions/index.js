245dde5126608420a1dce5db6fd9cb26a0770429
GustavoAF <gustavoafonso.gu@gmail.com>
2018-06-29 15:39:41 -0300

    inicio reestruturacao base/app

--- node server/functions/index.js
+++ node server/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const express = require('express');
+//const cors = require('cors');
 
 //var firebase = require("firebase");
 const admin = require("firebase-admin");
@@ -10,6 +11,7 @@ var db = admin.database();
 
 
 const app = express();
+//app.use(cors({ origin: true }));
 
 app.post('/timestamp', (request, response) => {
 
@@ -26,8 +28,10 @@ app.post('/timestamp', (request, response) => {
 		mm = '0' + mm;
 	}
 
-	today = dd + '/' + mm + '/' + yyyy;
+	dd = dd-1;
 
+	today = dd + '/' + mm + '/' + yyyy;
+	todaye = dd + '-' + mm + '-' + yyyy;
 
 	var d = new Date(Date.now());
 	d.setHours(d.getHours() - 3);
@@ -37,20 +41,58 @@ app.post('/timestamp', (request, response) => {
 	var seconds = d.getSeconds();
 
 	var tempo = horas + ':' + minutos + ':' + seconds;
+	var ref = db.ref("/alertas/" + dd + '-' + mm + '-' +
+		yyyy);
 
-	var ref = db.ref("/alertas/" + dd + '-' + mm + '-' + yyyy);
 
-	ref.push({
+	var confirmacao = db.ref("/Nodes").orderByChild("nodeMCU").equalTo(request.body.nodeMCU).once("value").then(snapshot => {
+		var confirma;
+		if (snapshot.exists()) {
+			ref.push({
+				nodeMCU: request.body.nodeMCU,
+				sensor: request.body.sensor,
+				exibido: false,
+				tempo: tempo,
+				data: today,
+				pasta: todaye
+			});
 
-		nodeMCU: request.body.nodeMCU,
-		sensor: request.body.sensor,
-		exibido: false,
-		tempo: tempo,
-		data: today
+			db.ref("/cadastrados").push({
+				nodeMCU: request.body.nodeMCU,
+				sensor: request.body.sensor,
+				exibido: false,
+				tempo: tempo,
+				data: today,
+				pasta: todaye
+			});
 
-	});
+			confirma = "SIM";
+		} else {
 
-	response.send('data: ' + today + ' hora: ' + tempo + ' Teste: ' + JSON.stringify(request.body));
+			db.ref("/nao_cadastrados").push({
+				nodeMCU: request.body.nodeMCU,
+				sensor: request.body.sensor,
+				exibido: false,
+				tempo: tempo,
+				data: today,
+				pasta: todaye
+			});
+
+			confirma = "NAO";
+		}
+		return confirma;
+
+	}).then(res => {
+		return response.send('data: ' + today + ' hora: ' + tempo + ' Teste: ' + JSON.stringify(request.body) + " Confirma: " + res);
+	});/* .finnaly(res => {
+		response.send('data: ' + today + ' hora: ' + tempo + ' Teste: ' + JSON.stringify(request.body) + " Confirma: ");
+	}); */
+	//var confirma = "teste";
+
+
+
+
+	//response.send('data: ' + today + ' hora: ' + tempo + ' Teste: ' + JSON.stringify(request.body) + " Confirma: " + confirma);
 });
 
 
@@ -93,8 +135,11 @@ app.get('/timestamp', (request, response) => {
 
 	});
 
-	response.send('data: ' + today + ' hora: ' + tempo);
+	response.send('data: ' + today + ' hora: ' + tempo + "TIPO: GET");
 });
+
+
+app.post('/', (req, res) => res.send("POST"));
 
 
 /*
@@ -108,6 +153,26 @@ app.get('/timestamp', (request, response) => {
 	
     response.send('sent');
 });*/
+
+/* function handleGET(req, res) {
+	// Do something with the GET request
+	res.status(200).send('Hello World!');
+}
+
+
+exports.helloHttp = (req, res) => {
+	switch (req.method) {
+		case 'GET':
+			handleGET(req, res);
+			break;
+		case 'POST':
+			res.status(500).send({ error: 'USING POST' });
+			break;
+		default:
+			res.status(500).send({ error: 'Something blew up!' });
+			break;
+	}
+}; */
 
 exports.app = functions.https.onRequest(app);
 
